70c5f9596f4f73824b3dc575c13564ec325e24df
unknown <jimmy.chirivi@labinfo.is.escuelaing.edu.co>
2020-02-13 15:27:15 -0500

    Recto 1 final

--- src/main/java/edu/escuelaing/arem/app/ServidorWeb.java
+++ src/main/java/edu/escuelaing/arem/app/ServidorWeb.java
@@ -1,6 +1,8 @@
 package edu.escuelaing.arem.app;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,17 +10,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 public class ServidorWeb {
 	
 	public static void main(String[] args) throws IOException {
 		ServerSocket serverSocket = null;
 		try {
-			serverSocket = new ServerSocket(8000);
+			serverSocket = new ServerSocket(getPort());
 		} catch (IOException e) {
 			System.err.println("Could not listen on port: 35000.");
 			System.exit(1);
@@ -35,19 +40,21 @@ public class ServidorWeb {
 		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		String inputLine, outputLine;
-		System.out.println(clientSocket.getInputStream());
-		System.out.println(in);
-		System.out.println(in.readLine());
-		String[] arch = null; 
+		String[] arch = null;
+		String[] elem = null;
+			
 		
 		while ((inputLine = in.readLine()) != null) {
-			System.out.println("Received: " + inputLine);
-			arch = inputLine.split("/");
-			if(! in.ready()) {
-				break;	
-			}
+		      System.out.println("Recibí: " + inputLine);
+		      if (inputLine.contains("GET")) {
+		    	  arch = inputLine.split("/");
+		    	  elem = arch[1].split(" ");  
+		      }
+		      if (!in.ready()) {break; }
 		}
-		out.println(Archivo(arch[1]));
+		
+		
+		Archivo(elem[0],clientSocket.getOutputStream(),out);
 		out.close();
 		in.close();
 		clientSocket.close();
@@ -56,48 +63,61 @@ public class ServidorWeb {
 	}
 	
 	
-	public static String Archivo(String a) throws IOException {
-		String [] archivo = a.split("."); 
+	public static void Archivo(String a,OutputStream clientOutput,PrintWriter out) throws IOException {
+		String [] archivo = a.split("\\."); 
 		String carpeta = "/src/resource/";
 		String outputLine = null;
 		File arch = new File(System.getProperty("user.dir")+carpeta + a);
 		if (arch.exists()) {
 			if (archivo[1].equals("html")) {
 				String lineas;
-				System.out.println("entra esta vuelta");
 				FileReader f = new FileReader(System.getProperty("user.dir")+carpeta + a);
 				BufferedReader bf = new BufferedReader(f);
 				while ((lineas = bf.readLine()) != null) {
-					System.out.println(lineas);
 					outputLine += lineas; 
 				}
 				
 				bf.close();
-				return outputLine;
-				/*FileInputStream filecall = new FileInputStream(arch);
-				byte[] datos = new byte [(int) arch.length()];
-				filecall.read(datos);
-				filecall.close();
-				DataOutputStream binout = new DataOutputStream(clientSocket.getOutputStream());
-				binout.writeBytes("HTTP/1.0 200 OK\r\n");
-				binout.writeBytes("Content-Type: text/html\r\n");
-				BufferedReader br = new BufferedReader(new FileReader(arch));
-				StringBuilder strB = new StringBuilder();
-				String inputLine = null;
-				while ((inputLine = br.readLine()) != null) {
-					strB.append(inputLine);
-				}
-				System.out.println(strB.toString());
-				br.close();*/
+				out.println("HTTP/1.1 200 OK\r\n"
+				        + "Content-Type: text/html\r\n"
+				         + "\r\n"+ outputLine); 
 			}
 			else if (archivo[1].equals("jpg") || archivo[1].equals("png")) {
 				
+				System.out.println("entra a la vuelta");
+				
+				BufferedImage imagen = ImageIO.read(arch);
+				
+				ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+				
+				DataOutputStream w = new DataOutputStream(clientOutput);
+				
+				ImageIO.write (imagen, archivo[1], bytes );
+				
+				
+				 w.writeBytes("HTTP/1.1 200 OK \r\n");
+		         w.writeBytes("Content-Type: image/"+archivo[1]+" \r\n");
+		         w.writeBytes("\r\n");
+		         w.write(bytes.toByteArray());
+				
+				System.out.println(System.getProperty("user.dir")+carpeta + a);				
+			
 			}
 			else {
 				
+				String lineas;
+				FileReader f = new FileReader(System.getProperty("user.dir")+carpeta + a);
+				BufferedReader bf = new BufferedReader(f);
+				while ((lineas = bf.readLine()) != null) {
+					outputLine += lineas; 
+				}
+				
+				bf.close();
+				out.println("HTTP/1.1 200 OK\r\n"
+				        + "Content-Type: text/html\r\n"
+				         + "\r\n"+ outputLine);
 			}
 		}
-		return outputLine;
 	}
 	
 	
@@ -106,7 +126,7 @@ public class ServidorWeb {
 	          return Integer.parseInt(System.getenv("PORT"));
 	      }        
 	         
-	      return 4759; //returns default port if heroku-port isn't set(i.e. on localhost)    }
+	      return 8080; //returns default port if heroku-port isn't set(i.e. on localhost)    }
 	  }
 	
 	
